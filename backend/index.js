bbfd31f448248bf5cc18a0db375cf12ba8a441b6
Vidhi Singh <vidhip.ug22.ec@nitp.ac.in>
2025-09-15 18:34:31 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,12 @@ const {mainRouter} = require('./routes/mainRouter');
 require('dotenv').config();  
 
 const allowedOrigins = [
-  "https://transactly-orpin.vercel.app", // your Vercel frontend
-  "http://localhost:3000"                // local dev frontend
+  "https://transactly-orpin.vercel.app", 
+  "https://transactly-6nnd.onrender.com"        
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps, curl, Postman)
     if (!origin) return callback(null, true);
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
